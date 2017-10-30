8fa5682f39cf53f10962b26deeb1b79da1b7887d
Alan Tsai <alan@alantsai.net>
2017-10-30 23:11:34 +0800

    console 使用 lib

--- src/DllHellProblem/Program.cs
+++ src/DllHellProblem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 using Newtonsoft.Json;
 
 namespace DllHellProblem
@@ -11,6 +12,9 @@ namespace DllHellProblem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(JsonConvert.SerializeObject(new { From = "Console"}));
+            Console.WriteLine(new Class1().Convert());
+            Console.ReadLine();
         }
     }
 }
